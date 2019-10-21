64a4c5fe42d65aa59b2343e038c816016d9542a3
oscarchan1996 <oscar.chan@barnett-waddingham.co.uk>
2019-10-21 21:00:36 +0100

    editfiles

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Overall functions create a matrix and sets cache, so that the second 
+## function calculating the inverse can skip computation
 
-## Write a short comment describing this function
+## This function sets the values for matrix and inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setInv <- function(solve) m <<- solve
+    getInv <- function() m
+    list(set = set, get = get,
+         setInv = setInv,
+         getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## This function calculates inverse of the matrix above, provided it has not yet been computed in local environment
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getInv()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setInv(m)
+    m
 }
